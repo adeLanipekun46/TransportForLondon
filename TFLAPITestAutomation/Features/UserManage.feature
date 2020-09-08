3295c73a4cb14910af481167b8f958c522ad0b27
adeolulanipekun <adeolulanipekun@hotmail.com>
2020-09-08 10:07:54 +0100

    added step definition file

--- TFLAPITestAutomation/Features/UserManage.feature
+++ TFLAPITestAutomation/Features/UserManage.feature
@@ -12,9 +12,24 @@ And created details should be
 | tony | leader |
 
 Scenario: Retrieve a single user
+Given GET request to create user with firstname 'Janet' and lastname 'Weaver' is sent
+Then the status code should be 201
+And created details should be
+| firstname | lastname      |
+| Janet      | Weaver       |
 
 Scenario: Retrieve list of users
 
+
 Scenario: Update single user
+Given put request to update user with name 'John' and job 'coach' is sent
+Then the status code should be 201
+And created details should be
+| name | job    |
+| John | coach  |
+
 
 Scenario: Retrieve unknown user
+Given GET request to create user with blankfirstname '  ' and blank lastname ' ' is sent
+Then the status code should be 404
+       |
---
+++ TFLAPITestAutomation/StepDefinitions/UserManageSteps.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using TechTalk.SpecFlow;
+
+namespace TFLAPITestAutomation.StepDefinitions
+{
+    [Binding]
+    public class UserManageSteps
+    {
+        [Given(@"post request to create user with name '(.*)' and job '(.*)' is sent")]
+        public void GivenPostRequestToCreateUserWithNameAndJobIsSent(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"GET request to create user with firstname '(.*)' and lastname '(.*)' is sent")]
+        public void GivenGETRequestToCreateUserWithFirstnameAndLastnameIsSent(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"put request to update user with name '(.*)' and job '(.*)' is sent")]
+        public void GivenPutRequestToUpdateUserWithNameAndJobIsSent(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Given(@"GET request to create user with blankfirstname '(.*)' and blank lastname '(.*)' is sent")]
+        public void GivenGETRequestToCreateUserWithBlankfirstnameAndBlankLastnameIsSent(string p0, string p1)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"the status code should be (.*)")]
+        public void ThenTheStatusCodeShouldBe(int p0)
+        {
+            ScenarioContext.Current.Pending();
+        }
+        
+        [Then(@"created details should be")]
+        public void ThenCreatedDetailsShouldBe(Table table)
+        {
+            ScenarioContext.Current.Pending();
+        }
+    }
+}
